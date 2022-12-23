688d5b99c08f962f727cfb1a0ea42cdc8cb0927d
Lukas Zwanepol <lukas zwanepol>
2022-12-23 08:51:52 +0100

    update styling toggle dark/light mode

--- Cart.php
+++ Cart.php
@@ -16,6 +16,7 @@ $StockGroups = getStockGroups($databaseConnection);
         $totalShoppingValue = 0;
         $cartItem =[];
         $verzendkosten = 0;
+<<<<<<< HEAD
 
         if (isset($_POST['DeleteKorting'])) {
             deletekorting($_POST['Kortingscode']);
@@ -26,6 +27,8 @@ $StockGroups = getStockGroups($databaseConnection);
             addkorting($_POST['Kortingscode']);
             print '<meta http-equiv="refresh" content="0">';
         }
+=======
+>>>>>>> 95f7e78 (update styling toggle dark/light mode)
 
         if ($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['increaseItem'])) {
             increaseAmountOfCart($_POST['increaseItem']);
@@ -71,6 +74,7 @@ $StockGroups = getStockGroups($databaseConnection);
                                                 <?php
                                             } ?>
                                         </ul>
+<<<<<<< HEAD
 
                                         <!-- slideshow -->
                                         <div class="carousel-inner">
@@ -82,6 +86,19 @@ $StockGroups = getStockGroups($databaseConnection);
                                             <?php } ?>
                                         </div>
 
+=======
+
+                                        <!-- slideshow -->
+                                        <div class="carousel-inner">
+                                            <?php for ($i = 0; $i < count($StockItemImage); $i++) {
+                                                ?>
+                                                <div class="carousel-item <?php print ($i == 0) ? 'active' : ''; ?>">
+                                                    <img src="Public/StockItemIMG/<?php print $StockItemImage[$i]['ImagePath'] ?>">
+                                                </div>
+                                            <?php } ?>
+                                        </div>
+
+>>>>>>> 95f7e78 (update styling toggle dark/light mode)
                                         <!-- knoppen 'vorige' en 'volgende' -->
                                         <a class="carousel-control-prev" href="#ImageCarousel" data-slide="prev">
                                             <span class="carousel-control-prev-icon"></span>
@@ -100,6 +117,7 @@ $StockGroups = getStockGroups($databaseConnection);
                             <?php
                         }
                         ?>
+<<<<<<< HEAD
                         <div id="ImageFrame"
                              style="background-image: url('Public/StockGroupIMG/<?php print $cartItem['BackupImagePath']; ?>'); background-size: cover; width:300px;"></div>
                         <?php
@@ -274,6 +292,96 @@ $StockGroups = getStockGroups($databaseConnection);
             } else {
                 print 1;
             } ?>'>Naar willekeurige artikelpagina</a></p>
+=======
+                        <h1 class="StockItemName" ; style=font-size:160%>
+                            <?php print $cartItem['StockItemName']; ?>
+                        </h1>
+                        <h2 class="StockItemIDa"
+                            style="font-size:80%; margin-top:2%"> <?php print ("Artikelnummer: $id"); ?>
+                        </h2>
+                        <div style="margin-top: 5%;">Voorraad: <?php print $cartItem['QuantityOnHand']; ?></div>
+                        <div id="StockItemHeaderLeft">
+                            <div class="CenterPriceLeft">
+                                <div class="CenterPriceLeftChild">
+                                    <p class="StockItemPriceText"
+                                    style="margin-top: -60px;font-family:pt-sans, sans-serif;">
+                                        <b><?php print sprintf("€ %.2f", $cartItem['SellPrice']); ?></b></p>
+                                    <h6 style="margin-top: -5%; font-size: 70%"> Inclusief BTW </h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-2 text-center">
+                        <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                            <div>
+                                <button style="background-color:#676EFF; border-radius: 12px; width: 30px; border: 1px rgba(35, 40, 47, 0.8); margin-top: 40px;"
+                                        type="submit" name="increaseItem" value="<?php print($id) ?>">+
+                                </button>
+                            </div>
+                            <div>
+                                <?php
+                                print($orderAmount);
+                                ?>
+                            </div>
+                            <div>
+                                <button style="background-color: #676EFF; border-radius: 12px;width: 30px;border: 1px rgba(35, 40, 47, 0.8);"
+                                        type="submit" name="decreaseItem" value="<?php print($id) ?>">-
+                                </button>
+                            </div>
+                            <div>
+                                    <button style="background-color: #676EFF; border-radius: 12px;width: 40px;border: 1px rgba(35, 40, 47, 0.8); margin: 30px;"
+                                            type="submit" name="deleteItem" value="<?php print($id) ?>" class="btn btn-light"><span class="bi bi-trash"></span>
+                                    </button>
+                            </div>
+                        </form>
+                        <div>
+                            <p>
+                                <?php
+                                $verzendkosten = ($cartItem['SendCosts']);
+                                print round($verzendkosten, 2);
+                                ?>
+                            </p>
+                            <p> Subtotaal: <?php
+                                $total = $orderAmount * $cartItem['SellPrice'];
+                                print round($total, 2); ?>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <?php
+                $totalShoppingValue += $total;
+            };
+            $verzendkosten = $cartItem['SendCosts'];
+        }else{ 
+        ?>
+            <h1>uw winkelwagen is leeg</h1>
+        <?php
+        }
+        ?>
+        <div class="row">
+            <div class="col-10"></div>
+            <div class="col-2">
+                <p class="text-center">Verzendkosten: <?php print (round($verzendkosten, 2)); ?></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-10"></div>
+            <div class="col-2">
+                <p class="text-center">Totaal: <?php print (round($totalShoppingValue , 2) + $verzendkosten ); ?></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-10"></div>
+            <div class="col-2 text-center">
+                <?php if ($totalShoppingValue != 0) { ?>
+                    <a href="gegevens.php" class="btn btn-primary btn-lg">Naar checkout</a>
+                <?php 
+                } 
+                ?>
+            </div>
+        </div>
+
+>>>>>>> 95f7e78 (update styling toggle dark/light mode)
 
     </div>
 </div>
